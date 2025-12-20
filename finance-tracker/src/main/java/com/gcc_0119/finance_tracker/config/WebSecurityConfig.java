385f93ea54910184a0aaf197bf3ec9e40082a66d
mcheiyue <79710980+mcheiyue@users.noreply.github.com>
2025-12-20 20:15:15 +0800

    Fix：饼图组件高亮重置问题

--- finance-tracker/src/main/java/com/gcc_0119/finance_tracker/config/WebSecurityConfig.java
+++ finance-tracker/src/main/java/com/gcc_0119/finance_tracker/config/WebSecurityConfig.java
@@ -15,9 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
 
-import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
